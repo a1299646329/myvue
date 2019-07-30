e75b0babf9026646004e775433a59a166d8d47b1
zlcao <caozlb@wumart.com>
2019-07-30 15:05:14 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <button @click="aaaaa">sasa</button>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+export default {
+  name: 'App',
+  methods:{
+    aaaaa (){
+      axios.get("http://127.0.0.1:5000").then(function(r) {
+        console.log(r.data);
+        alert(r.data.message);
+      });
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
